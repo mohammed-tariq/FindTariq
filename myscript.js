8a9cfacd251916fa2611c5872958a93435bd22e6
MTariq <tariqsheik786@gmail.com>
2019-08-26 11:12:37 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,19 @@
+$(function(){
+  $("#header").load("header.html"); 
+});
+
+function showMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("shwmore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
